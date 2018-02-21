430ce0343439f3f5497e27ede306dc687f222544
ITMASTER\a.avola <a.avola@almaviva.it>
2018-02-21 14:04:06 +0100

    eliminati riferimenti a componenti aggiuntivi "forward" e "back to top" (da inserire in seguito)

--- docs/esempi/comuni/script.js
+++ docs/esempi/comuni/script.js
@@ -1,16 +1,4 @@
-// ===== Scroll to Top ====
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
+
 $(function(){
   $('.modal').modal('show')
 })
