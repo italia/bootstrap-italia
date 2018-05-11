d97a674c0d673627146004a32546f857117ebaf7
Oscar Bresolin <o.bresolin@wedoo.it>
2018-05-11 11:33:56 +0200

    dropdown animation

--- src/js/plugins/navbar.js
+++ src/js/plugins/navbar.js
@@ -61,3 +61,15 @@ $(closebutton).blur(function(event) {
 		}, 50)
 	}
 });
+
+
+//-- Animazione dropdown
+// Animazione slide down
+$('.dropdown').on('show.bs.dropdown', function() {
+	$(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+});
+
+// Animazione slide up
+$('.dropdown').on('hide.bs.dropdown', function() {
+	$(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
+});
