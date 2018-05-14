bb50f46be2159e4648487d75a487d675bee08d67
Oscar Bresolin <o.bresolin@wedoo.it>
2018-05-14 15:19:38 +0200

    review dropdown animations

--- src/js/plugins/dropdown.js
+++ src/js/plugins/dropdown.js
@@ -1,10 +1,22 @@
-//-- Animazione dropdown
+//-- Animazione dropdown mobile
+
 // Animazione slide down
 $('.navbar .dropdown').on('show.bs.dropdown', function() {
-	$(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+	if (window.matchMedia("(max-width: 768px)").matches) {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+	}
 });
 
 // Animazione slide up
 $('.navbar .dropdown').on('hide.bs.dropdown', function() {
-	$(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
+	if (window.matchMedia("(max-width: 768px)").matches) {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
+	}
 });
+
+$( window ).resize(function() {
+	// reset parametri click iniettati su mobile
+	if (window.matchMedia("(min-width: 769px)").matches) {
+		$('.navbar .dropdown-menu.show').removeAttr('style')
+	}
+})
