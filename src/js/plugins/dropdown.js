bcb8613987d4b0dd3459fd4aece29a61f37841ef
Oscar Bresolin <o.bresolin@wedoo.it>
2018-09-21 09:12:21 +0200

    megamenu first step

--- src/js/plugins/dropdown.js
+++ src/js/plugins/dropdown.js
@@ -2,21 +2,24 @@
 
 // Animazione slide down
 $('.navbar .dropdown').on('show.bs.dropdown', function() {
-	if (window.matchMedia("(max-width: 768px)").matches) {
+	if (window.matchMedia("(max-width: 992px)").matches) {
 		$(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+		console.log('Sliode down')
 	}
 });
 
 // Animazione slide up
 $('.navbar .dropdown').on('hide.bs.dropdown', function() {
-	if (window.matchMedia("(max-width: 768px)").matches) {
+	if (window.matchMedia("(max-width: 992px)").matches) {
 		$(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
+		console.log('Slide up')
 	}
 });
 
 $( window ).resize(function() {
 	// reset parametri click iniettati su mobile
-	if (window.matchMedia("(min-width: 769px)").matches) {
+	if (window.matchMedia("(min-width: 993px)").matches) {
 		$('.navbar .dropdown-menu.show').removeAttr('style')
+		console.log('resize')
 	}
 })
