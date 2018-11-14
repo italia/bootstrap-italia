21b978a808e465d03722ced364b4aff3fc7e1f89
Oscar Bresolin <o.bresolin@wedoo.it>
2018-11-14 17:46:19 +0100

    select end

--- src/js/plugins/select.js
+++ src/js/plugins/select.js
@@ -1,3 +1,26 @@
 $( document ).ready(function() {
+	// change checkbox states in dropdown
+	$(function () {
+		$(".selectpicker").on("changed.bs.select", function(e, clickedIndex, newValue, oldValue) {
+		/*	var selectedD = $(this).find('option').eq(clickedIndex).attr("check-id");
+			var toChange = $('.dropdown-menu').find('input[data-id=' + selectedD +']');
 
+			if ($(toChange).prop('checked')){
+				$(toChange).prop('checked',false)
+			}else{
+				$(toChange).prop('checked',true)
+			}*/
+		});
+	});
+
+	$(function () {
+		$(".selectpicker").on("changed.bs.select", function(e){
+			var tocheck = $('.dropdown-menu li.selected').find('input[type="checkbox"]');
+			var todecheck = $('.dropdown-menu li:not(.selected)').find('input[type="checkbox"]');
+			$(tocheck).prop('checked',true)
+			$(todecheck).prop('checked',false)
+		});
+	});
+
+	console.log('new')
 });
