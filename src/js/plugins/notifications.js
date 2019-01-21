108570928b88592a21432fa02c48a15baff9c0e1
Francesco Zaia <francesco.zaia@gmail.com>
2019-01-21 12:26:11 +0100

    Template vuoto di esempio (#266)
    * Esempio di template vuoto
    
    * Nuova pagina template di esempio, aggiunta una notifica in tutte le pagine di esempio per agevolare la navigazione, aggiunto footer CSS
    
    * A11Y fix

--- src/js/plugins/notifications.js
+++ src/js/plugins/notifications.js
@@ -1,26 +1,25 @@
-function notificationShow(notificationTarget,notificationTimeOut) {
+function notificationShow(notificationTarget, notificationTimeOut) {
 
-	if ($('#'+notificationTarget).hasClass('dismissable')) {
-		//dismissable
-		$('#'+notificationTarget).fadeIn(300);
-	} else {
-		//standard (timeout)
-		$('#'+notificationTarget).fadeIn(300);
-		if (typeof notificationTimeOut == "number") {
-			//timeout set by parameter
-			var timeToFade = notificationTimeOut;
-		} else {
-			//timeout default value 7s
-			var timeToFade = 7000;
-		}
-		//fadeout
-		setTimeout(function(){
-			$('#'+notificationTarget).fadeOut(100);
-		}, timeToFade);
-	}
+  $('#' + notificationTarget).fadeIn(300);
+
+  if (!$('#' + notificationTarget).hasClass('dismissable')) {
+    //standard (timeout)
+    $('#' + notificationTarget).fadeIn(300);
+    if (typeof notificationTimeOut == "number") {
+      //timeout set by parameter
+      var timeToFade = notificationTimeOut;
+    } else {
+      //timeout default value 7s
+      var timeToFade = 7000;
+    }
+    //fadeout
+    setTimeout(function () {
+      $('#' + notificationTarget).fadeOut(100);
+    }, timeToFade);
+  }
 }
 
 //dismissable close button
-$('.notification-close').click(function(){
-	$(this).closest('.notification').fadeOut(100);
+$(document).on('click', '.notification-close', function () {
+  $(this).closest('.notification').fadeOut(100);
 });
