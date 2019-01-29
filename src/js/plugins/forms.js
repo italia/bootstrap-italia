0257a0c4395f946ecfae1499910e089a90b99ad9
Avol.io <social@avol.io>
2019-01-29 10:59:07 +0100

    gestisco i casi in cui il placeholder è stringa vuota

--- src/js/plugins/forms.js
+++ src/js/plugins/forms.js
@@ -1,39 +1,45 @@
-$(function () {
-
-  const inputSelector = `${['text', 'password', 'email', 'url', 'tel', 'number', 'search']
-    .map((selector) => `input[type=${selector}]`)
+$(function() {
+  const inputSelector = `${[
+    'text',
+    'password',
+    'email',
+    'url',
+    'tel',
+    'number',
+    'search'
+  ]
+    .map(selector => `input[type=${selector}]`)
     .join(', ')}, textarea`;
 
   const inputFileSelector = `input[type="file"]`;
 
-  $(document).on('focus', inputSelector, (e) => {
+  $(document).on('focus', inputSelector, e => {
     const $this = $(e.target);
     $this.siblings('label, i').addClass('active');
   });
 
-  $(document).on('blur', inputSelector, (e) => {
+  $(document).on('blur', inputSelector, e => {
     const $this = $(e.target);
     const noValue = !$this.val();
-    const noPlaceholder = $this.attr('placeholder') === undefined;
+    const noPlaceholder = !$this.attr('placeholder'); //! gestice sia undefined che '' che null
 
     if (noValue && noPlaceholder) {
       $this.siblings('label, i').removeClass('active');
     }
-
   });
 
-  $(document).on('change', inputSelector, (e) => {
+  $(document).on('change', inputSelector, e => {
     const $this = $(e.target);
     updateTextFields($this);
     validateField($this);
   });
 
-  $(document).on('blur', inputFileSelector, (e) => {
+  $(document).on('blur', inputFileSelector, e => {
     const $this = $(e.target);
     $this.siblings('label').addClass('active');
   });
 
-  $(document).on('change', inputFileSelector, (e) => {
+  $(document).on('change', inputFileSelector, e => {
     const $this = $(e.target);
     var numFiles = e.currentTarget.files.length;
     var nomiFiles = '';
@@ -41,23 +47,24 @@ $(function () {
     for (i = 0; i < numFiles; i++) {
       fileSize = parseInt(e.currentTarget.files[i].size, 10) / 1024;
       filesize = Math.round(fileSize);
-      nomiFiles = nomiFiles + e.currentTarget.files[i].name + ' (' + filesize + 'kb); ';
+      nomiFiles =
+        nomiFiles + e.currentTarget.files[i].name + ' (' + filesize + 'kb); ';
     }
     if (numFiles > 1) {
-      multi = numFiles + ' file da caricare: '
+      multi = numFiles + ' file da caricare: ';
     }
     $this.siblings('.form-file-name').text(multi + nomiFiles);
   });
 
-  const updateTextFields = ($input) => {
+  const updateTextFields = $input => {
     const $labelAndIcon = $input.siblings('label, i');
     const hasValue = $input.val().length;
-    const hasPlaceholder = $input.attr('placeholder');
+    const hasPlaceholder = !!$input.attr('placeholder'); //!! gestice sia undefined che '' che null
     const addOrRemove = `${hasValue || hasPlaceholder ? 'add' : 'remove'}Class`;
     $labelAndIcon[addOrRemove]('active');
   };
 
-  const validateField = ($input) => {
+  const validateField = $input => {
     if ($input.hasClass('validate')) {
       const value = $input.val();
       const noValue = !value.length;
@@ -76,7 +83,8 @@ $(function () {
     }
   };
 
-  $('body').find(inputSelector)
+  $('body')
+    .find(inputSelector)
     .removeClass('valid')
     .removeClass('invalid')
     .each((index, input) => {
@@ -84,9 +92,10 @@ $(function () {
       const hasDefaultValue = !!$this.val();
       const hasPlaceholder = !!$this.attr('placeholder');
       if (hasDefaultValue || hasPlaceholder) {
-        $this.siblings('label, i')
+        $this
+          .siblings('label, i')
           .css('transition', 'none')
-          .addClass('active')
+          .addClass('active');
       }
     });
 
