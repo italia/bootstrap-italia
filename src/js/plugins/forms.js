02eecb8340a4c5d49c2a09c3b068cd870ebbc7d4
Avol.io <social@avol.io>
2019-01-30 17:16:56 +0100

    Pulizia di tutti i file in src + aggiunta hook pre-commit per prettify i nuovi file (#272)
    * aggiunto vscode a gitignore
    
    * prettify  di tutti i file js
    
    * prettify tutti i file di scss
    
    * aggiunto pre-commit hook preso dal repository di prettier
    
    * fix su pre-commit

--- src/js/plugins/forms.js
+++ src/js/plugins/forms.js
@@ -8,93 +8,92 @@ $(function() {
     'input[type="tel"],' +
     'input[type="number"],' +
     'input[type="search"],' +
-    'textarea';
+    'textarea'
 
-  const inputFileSelector = 'input[type="file"]';
+  const inputFileSelector = 'input[type="file"]'
 
   $(document).on('focus', inputSelector, e => {
-    const $this = $(e.target);
-    $this.siblings('label, i').addClass('active');
-  });
+    const $this = $(e.target)
+    $this.siblings('label, i').addClass('active')
+  })
 
   $(document).on('blur', inputSelector, e => {
-    const $this = $(e.target);
-    const noValue = !$this.val();
-    const noPlaceholder = !$this.attr('placeholder');
+    const $this = $(e.target)
+    const noValue = !$this.val()
+    const noPlaceholder = !$this.attr('placeholder')
 
     if (noValue && noPlaceholder) {
-      $this.siblings('label, i').removeClass('active');
+      $this.siblings('label, i').removeClass('active')
     }
-  });
+  })
 
   $(document).on('change', inputSelector, e => {
-    const $this = $(e.target);
-    updateTextFields($this);
-    validateField($this);
-  });
+    const $this = $(e.target)
+    updateTextFields($this)
+    validateField($this)
+  })
 
   $(document).on('blur', inputFileSelector, e => {
-    const $this = $(e.target);
-    $this.siblings('label').addClass('active');
-  });
+    const $this = $(e.target)
+    $this.siblings('label').addClass('active')
+  })
 
   $(document).on('change', inputFileSelector, e => {
-    const $this = $(e.target);
-    var numFiles = e.currentTarget.files.length;
-    var nomiFiles = '';
-    var multi = '';
+    const $this = $(e.target)
+    var numFiles = e.currentTarget.files.length
+    var nomiFiles = ''
+    var multi = ''
     for (i = 0; i < numFiles; i++) {
-      fileSize = parseInt(e.currentTarget.files[i].size, 10) / 1024;
-      filesize = Math.round(fileSize);
+      fileSize = parseInt(e.currentTarget.files[i].size, 10) / 1024
+      filesize = Math.round(fileSize)
       nomiFiles =
-        nomiFiles + e.currentTarget.files[i].name + ' (' + filesize + 'kb); ';
+        nomiFiles + e.currentTarget.files[i].name + ' (' + filesize + 'kb); '
     }
     if (numFiles > 1) {
-      multi = numFiles + ' file da caricare: ';
+      multi = numFiles + ' file da caricare: '
     }
-    $this.siblings('.form-file-name').text(multi + nomiFiles);
-  });
+    $this.siblings('.form-file-name').text(multi + nomiFiles)
+  })
 
   const updateTextFields = $input => {
-    const $labelAndIcon = $input.siblings('label, i');
-    const hasValue = $input.val().length;
-    const hasPlaceholder = !!$input.attr('placeholder');
-    $labelAndIcon.toggleClass('active', hasValue || hasPlaceholder);
-  };
+    const $labelAndIcon = $input.siblings('label, i')
+    const hasValue = $input.val().length
+    const hasPlaceholder = !!$input.attr('placeholder')
+    $labelAndIcon.toggleClass('active', hasValue || hasPlaceholder)
+  }
 
   const validateField = $input => {
     if ($input.hasClass('validate')) {
-      const value = $input.val();
-      const noValue = !value.length;
-      const isValid = !$input[0].validity.badInput;
+      const value = $input.val()
+      const noValue = !value.length
+      const isValid = !$input[0].validity.badInput
       if (noValue && isValid) {
-        $input.removeClass('valid').removeClass('invalid');
+        $input.removeClass('valid').removeClass('invalid')
       } else {
-        const valid = $input.is(':valid');
-        const length = Number($input.attr('length')) || 0;
+        const valid = $input.is(':valid')
+        const length = Number($input.attr('length')) || 0
         if (valid && (!length || length > value.length)) {
-          $input.removeClass('invalid').addClass('valid');
+          $input.removeClass('invalid').addClass('valid')
         } else {
-          $input.removeClass('valid').addClass('invalid');
+          $input.removeClass('valid').addClass('invalid')
         }
       }
     }
-  };
+  }
 
   $('body')
     .find(inputSelector)
     .removeClass('valid')
     .removeClass('invalid')
     .each((index, input) => {
-      const $this = $(input);
-      const hasDefaultValue = !!$this.val();
-      const hasPlaceholder = !!$this.attr('placeholder');
+      const $this = $(input)
+      const hasDefaultValue = !!$this.val()
+      const hasPlaceholder = !!$this.attr('placeholder')
       if (hasDefaultValue || hasPlaceholder) {
         $this
           .siblings('label, i')
           .css('transition', 'none')
-          .addClass('active');
+          .addClass('active')
       }
-    });
-
-});
+    })
+})
--- src/js/plugins/navbar.js
+++ src/js/plugins/navbar.js
@@ -1,78 +1,96 @@
 // open / close navbar actions
-var openbutton = $('.custom-navbar-toggler');
-var closebutton = $('.close-div ');
-var overlay = $('.overlay');
-var backbutton = $('.it-back-button');
-var navlink = $('.navbar-collapsable a');
+var openbutton = $('.custom-navbar-toggler')
+var closebutton = $('.close-div ')
+var overlay = $('.overlay')
+var backbutton = $('.it-back-button')
+var navlink = $('.navbar-collapsable a')
 
 //-open button action
-$(openbutton).on('click', function (event) {
+$(openbutton).on('click', function(event) {
   /* Act on the event */
-  var target = $(this).attr('data-target');
-  var fadelayer = $(target).find('.overlay');
-  $(this).attr('aria-expanded', 'true');
-  $(backbutton).fadeIn();
-  $(target).show();
-  $(fadelayer).fadeIn();
-  $(target).addClass('expanded');
-});
+  var target = $(this).attr('data-target')
+  var fadelayer = $(target).find('.overlay')
+  $(this).attr('aria-expanded', 'true')
+  $(backbutton).fadeIn()
+  $(target).show()
+  $(fadelayer).fadeIn()
+  $(target).addClass('expanded')
+})
 
-$(overlay).on('click', function () {
-  var target = $(this).closest('.navbar-collapsable');
-  var buttonrel = $(this).closest('.navbar').find('.custom-navbar-toggler');
-  var fadelayer = $(target).find('.overlay');
-  $(buttonrel).attr('aria-expanded', 'false');
+$(overlay).on('click', function() {
+  var target = $(this).closest('.navbar-collapsable')
+  var buttonrel = $(this)
+    .closest('.navbar')
+    .find('.custom-navbar-toggler')
+  var fadelayer = $(target).find('.overlay')
+  $(buttonrel).attr('aria-expanded', 'false')
 
-  $(target).removeClass('expanded');
-  $(fadelayer).fadeOut();
-  setTimeout(function () {
-    $(target).hide();
-  }, 300);
-});
+  $(target).removeClass('expanded')
+  $(fadelayer).fadeOut()
+  setTimeout(function() {
+    $(target).hide()
+  }, 300)
+})
 
 //-close button action
-$(closebutton).on('click', function (event) {
-  var target = $(this).closest('.navbar-collapsable');
-  var buttonrel = $(this).closest('.navbar').find('.custom-navbar-toggler');
-  var fadelayer = $(target).find('.overlay');
+$(closebutton).on('click', function(event) {
+  var target = $(this).closest('.navbar-collapsable')
+  var buttonrel = $(this)
+    .closest('.navbar')
+    .find('.custom-navbar-toggler')
+  var fadelayer = $(target).find('.overlay')
 
-  $(buttonrel).attr('aria-expanded', 'false');
+  $(buttonrel).attr('aria-expanded', 'false')
 
-  $(target).removeClass('expanded');
-  $(fadelayer).fadeOut();
-  setTimeout(function () {
-    $(target).hide();
-  }, 300);
-});
+  $(target).removeClass('expanded')
+  $(fadelayer).fadeOut()
+  setTimeout(function() {
+    $(target).hide()
+  }, 300)
+})
 
 //-- (a fine elenco viene chiuso il menù ed il focus passa all'elemento successivo)
-$(navlink).on('blur', function (event) {
-  closemenu = $(this).closest('.navbar-collapsable').find('.close-div .btn'); // determino il pulsante di chiusura
+$(navlink).on('blur', function(event) {
+  closemenu = $(this)
+    .closest('.navbar-collapsable')
+    .find('.close-div .btn') // determino il pulsante di chiusura
   /* Act on the event */
-  if ($(this).closest('.navbar-collapsable').hasClass('expanded')) { // se il navigatore è a perto
-    setTimeout(function () {
-      var active = document.activeElement; // determino quale elemento ha il focus
-      var isMenu = $(active).closest('.navbar-collapsable').length; // controllo che l'elemento si trovi all'interno del navigatore
+  if (
+    $(this)
+      .closest('.navbar-collapsable')
+      .hasClass('expanded')
+  ) {
+    // se il navigatore è a perto
+    setTimeout(function() {
+      var active = document.activeElement // determino quale elemento ha il focus
+      var isMenu = $(active).closest('.navbar-collapsable').length // controllo che l'elemento si trovi all'interno del navigatore
 
-      if (isMenu == 0) { // se l'elemento è fuori dal navigatore
+      if (isMenu == 0) {
+        // se l'elemento è fuori dal navigatore
         $(closemenu).trigger('click') // simulo il click sul pulsante di chiusura del pannello
       }
-    }, 50);
+    }, 50)
   }
-});
+})
 
 //-- (a inizio elenco tabbando indietro, dopo il close chiudo il menu)
-$(closebutton).on('blur', function (event) {
-  closemenu = $(this);
+$(closebutton).on('blur', function(event) {
+  closemenu = $(this)
   /* Act on the event */
-  if ($(this).closest('.navbar-collapsable').hasClass('expanded')) { // se il navigatore è a perto
-    setTimeout(function () {
-      var active = document.activeElement; // determino quale elemento ha il focus
-      var isMenu = $(active).closest('.navbar-collapsable').length; // controllo che l'elemento si trovi all'interno del navigatore
+  if (
+    $(this)
+      .closest('.navbar-collapsable')
+      .hasClass('expanded')
+  ) {
+    // se il navigatore è a perto
+    setTimeout(function() {
+      var active = document.activeElement // determino quale elemento ha il focus
+      var isMenu = $(active).closest('.navbar-collapsable').length // controllo che l'elemento si trovi all'interno del navigatore
 
-      if (isMenu == 0) { // se l'elemento è fuori dal navigatore
+      if (isMenu == 0) {
+        // se l'elemento è fuori dal navigatore
         $(closemenu).trigger('click') // simulo il click sul pulsante di chiusura del pannello
       }
-    }, 50);
+    }, 50)
   }
-});
+})
