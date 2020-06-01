3eacd2d6a50660c3be9723ddb2b238c531f917eb
Francesco Zaia <francesco.zaia@gmail.com>
2020-06-01 16:49:36 +0200

    Uses local libraries, adds tsParticles, updates fews deps

--- docs/assets/src/js/docs.js
+++ docs/assets/src/js/docs.js
@@ -147,21 +147,20 @@ $(function () {
       .tooltip('_fixTitle')
   })
 
-  // ParticlesJS - Docs homepage particles
-  if ($('#particles-js').length) {
+  // tsParticles - Docs homepage particles
+  if ($('#tsparticles').length) {
     var particlesJSON = {
-      "particles": {
-        "number":{"value":160,"density":{"enable":true,"value_area":800}},
-        "color":{"value":"#FFF"},
-        "shape":{"type":"circle","stroke":{"width":0,"color":"#000000"}},
-        "opacity":{"value":0.8,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},
-        "size":{"value":2,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},
-        "line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.1,"width":1},
-        "move":{"enable":true,"speed":0.2,"direction":"top","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}
+      particles: {
+        number:{value:160,density:{enable:true,value_area:800}},
+        color:{value:"#FFF"},
+        shape:{type:"circle"},
+        opacity:{value:0.8,random:true,anim:{enable:true,speed:1,opacity_min:0,sync:false}},
+        size:{value:2,random:true},
+        move:{enable:true,speed:0.2,direction:top,random:true,straight:false,out_mode:"out",bounce:false}
       },
-      "retina_detect":true
+      retina_detect:true
     }
-    particlesJS('particles-js', particlesJSON);
+    tsParticles.load('tsparticles', particlesJSON);
   }
 
 });
