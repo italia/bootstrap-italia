99e7a05d381c3f6e8e861e9196aa3b952a0681d0
Oscar Bresolin <oscareb@gmail.com>
2022-01-24 15:46:17 +0100

    fix(core): lint js-scss

--- src/js/plugins/datepicker-validation.js
+++ src/js/plugins/datepicker-validation.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   let selector = $('.it-datepicker-wrapper')
   if (selector.length > 0) {
-    selector.each(function(){
+    selector.each(function () {
       let inputElem = $(this).find('.input-group').find('.it-date-datepicker')
       let alertdiv = $(this).find('.invalid-feedback')
       let successdiv = $(this).find('.valid-feedback')
