215d2da295d3ad3daebcca7793533e2392b4e459
Oscar Bresolin <oscareb@gmail.com>
2022-02-02 16:12:51 +0100

    feat(core,docs): lasts splide carousel

--- src/js/plugins/carousel.js
+++ src/js/plugins/carousel.js
@@ -15,8 +15,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var arrowsMob
     var arrowsTab
     var arrowsDesk
+    var cType
     Array.prototype.forEach.call(selector, function (el) {
       if (el.classList.contains('it-carousel-landscape-abstract-three-cols')) {
+        cType = 'slide'
         perPageMob = 1
         perPageTab = 2
         perPageDesk = 3
@@ -29,7 +31,31 @@ document.addEventListener('DOMContentLoaded', function () {
         arrowsMob = false
         arrowsTab = false
         arrowsDesk = false
+        if (el.classList.contains('it-big-img')) {
+          cType = 'loop'
+          perPageMob = 1
+          perPageTab = 1
+          perPageDesk = 1
+          gapMob = 0
+          gapTab = 24
+          gapDesk = 48
+          paddingMob = { left: 0, right: 0 }
+          paddingTab = { left: 160, right: 160 }
+          paddingDesk = { left: 320, right: 320 }
+        } else if (el.classList.contains('it-standard-image')) {
+          cType = 'loop'
+          perPageMob = 1
+          perPageTab = 2
+          perPageDesk = 3
+          gapMob = 24
+          gapTab = 24
+          gapDesk = 24
+          paddingMob = { left: 40, right: 40 }
+          paddingTab = { left: 48, right: 48 }
+          paddingDesk = { left: 48, right: 48 }
+        }
       } else if (el.classList.contains('it-carousel-landscape-abstract')) {
+        cType = 'slide'
         perPageMob = 1
         perPageTab = 1
         perPageDesk = 1
@@ -46,6 +72,7 @@ document.addEventListener('DOMContentLoaded', function () {
       /*eslint-disable */
       splide = new Splide(el, {
         /*eslint-enable */
+        type: cType,
         perPage: perPageDesk,
         gap: gapDesk,
         padding: paddingDesk,
