207632d1e769ef6b34b0a21b12569ba02f330f44
Gabriele Peirolo <g.peirolo@wedoo.it>
2022-02-07 17:06:50 +0100

    fix(core): comment on boostrap italia version css and js variables

--- src/js/plugins/version.js
+++ src/js/plugins/version.js
@@ -1,3 +1,4 @@
-// bootstrap italia version for crawler
+// bootstrap italia version variable
+// useful to check for the current version
 // eslint-disable-next-line no-unused-vars
 var BootstrapItaliaVersion = '1.5.0'
