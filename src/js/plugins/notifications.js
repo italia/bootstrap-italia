971dd669138b3a7ce3cc8048102f979f846ec854
Oscar Bresolin <oscareb@gmail.com>
2022-02-08 10:34:45 +0100

    fix(core,docs): removed commented code

--- src/js/plugins/notifications.js
+++ src/js/plugins/notifications.js
@@ -1,22 +1,24 @@
-/*function notificationShow(notificationTarget, notificationTimeOut) {
+/*eslint-disable */
+function notificationShow(notificationTarget, notificationTimeOut) {
+  /*eslint-disable */
   $('#' + notificationTarget).fadeIn(300)
-
+  var timeToFade
   if (!$('#' + notificationTarget).hasClass('dismissable')) {
     //standard (timeout)
     $('#' + notificationTarget).fadeIn(300)
     if (typeof notificationTimeOut == 'number') {
       //timeout set by parameter
-      var timeToFade = notificationTimeOut
+      timeToFade = notificationTimeOut
     } else {
       //timeout default value 7s
-      var timeToFade = 7000
+      timeToFade = 7000
     }
     //fadeout
     setTimeout(function () {
       $('#' + notificationTarget).fadeOut(100)
     }, timeToFade)
   }
-}*/
+}
 
 //dismissable close button
 $(document).on('click', '.notification-close', function () {
