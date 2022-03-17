90a0e5f1087b6cfc6506acc0a2d1310a5d87742e
Massimo Marsan <m.marsan@wedoo.it>
2022-03-17 10:52:02 +0100

    chore(core): import css for accessible autocomplete select, removed form validate message from fieldset

--- src/js/plugins/select-autocomplete.js
+++ src/js/plugins/select-autocomplete.js
@@ -1,3 +1,4 @@
+import 'accessible-autocomplete/dist/accessible-autocomplete.min.css'
 import accessibleAutocomplete from 'accessible-autocomplete'
 
 import BaseComponent from 'bootstrap/js/src/base-component.js'
