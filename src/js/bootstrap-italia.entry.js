4db28771b7d524a4ebfe922f508ba690d175b77d
Andrea Stagi <stagi.andrea@gmail.com>
2022-03-22 16:06:08 +0100

    chore: add esm export

--- src/js/bootstrap-italia.entry.js
+++ src/js/bootstrap-italia.entry.js
@@ -4,26 +4,12 @@ import { Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover,
 import './plugins/forward'
 import './plugins/fonts-loader'
 
-//import accessibleAutocomplete from 'accessible-autocomplete'
 
-import Dimmer from './plugins/dimmer'
-import Notification from './plugins/notification'
-import Cookiebar from './plugins/cookiebar'
-import NavBarCollapsible from './plugins/navbar-collapsible'
-import Accordion from './plugins/accordion'
-import NavScroll from './plugins/navscroll'
-import HeaderSticky from './plugins/header-sticky'
-//import Sticky from './plugins/sticky'
-import TrackFocus from './plugins/track-focus'
-import CarouselBI from './plugins/carousel-bi'
-import { FormValidate, ValidatorSelectAutocomplete } from './plugins/form-validate'
-import Input from './plugins/input'
-import SelectAutocomplete from './plugins/select-autocomplete'
-import InputSearchAutocomplete from './plugins/input-search-autocomplete'
-import InputPassword from './plugins/input-password'
-import InputNumber from './plugins/input-number'
-import ProgressDonut from './plugins/progress-donut'
-import UploadDragDrop from './plugins/upload-dragdrop'
+import {
+  Dimmer, Notification, Cookiebar, NavBarCollapsible, Accordion, NavScroll, HeaderSticky,
+  TrackFocus, CarouselBI, FormValidate, ValidatorSelectAutocomplete, Input, SelectAutocomplete,
+  InputSearchAutocomplete, InputPassword, InputNumber, ProgressDonut, UploadDragDrop
+} from './bootstrap-italia'
 
 import './plugins/inputs'
 
