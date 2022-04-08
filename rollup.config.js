ac62380a5f72f24e74b22a284fd5f36fd32a9993
Oscar Bresolin <oscareb@gmail.com>
2022-04-08 11:47:57 +0200

    chore: remove masonry copy

--- rollup.config.js
+++ rollup.config.js
@@ -27,7 +27,6 @@ export default [
         targets: [
           { src: 'src/assets', dest: 'dist' },
           { src: 'src/fonts', dest: 'dist' },
-          { src: 'node_modules/masonry-layout/dist/masonry.pkgd.min.js', dest: 'dist/js/vendor' },
         ],
       }),
       svgSprite({
