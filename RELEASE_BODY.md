170c8bde307edc48c5ad1ca643bcea4bce1d8c3a
astagi <astagi@users.noreply.github.com>
2022-10-05 12:43:53 +0000

    chore: update documentation

---
+++ RELEASE_BODY.md
@@ -0,0 +1,5 @@
+## [2.0.5](https://github.com/italia/bootstrap-italia/compare/v2.0.4...v2.0.5) (2022-10-05)
+
+### Bug Fixes
+
+* **core:** steppers active element on desktop ([287dd99](https://github.com/italia/bootstrap-italia/commit/287dd993813532ffc9abbe7e83ee0592d61742f9))
--- dist/version.js
+++ dist/version.js
@@ -1,7 +1,7 @@
 // bootstrap italia version variable
 // useful to check for the current version
 // eslint-disable-next-line no-unused-vars
-const BOOTSTRAP_ITALIA_VERSION = '2.0.4';
+const BOOTSTRAP_ITALIA_VERSION = '2.0.5';
 
 export { BOOTSTRAP_ITALIA_VERSION as default };
 //# sourceMappingURL=version.js.map
