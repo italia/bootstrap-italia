b39652345137376a602fb838b5bf597d4f4b24aa
Andrea Stagi <stagi.andrea@gmail.com>
2022-12-31 18:48:13 +0100

    fix: move splide and autocomplete css outside js

--- src/js/plugins/carousel-bi.js
+++ src/js/plugins/carousel-bi.js
@@ -1,5 +1,4 @@
 import Splide from '@splidejs/splide'
-import '@splidejs/splide/dist/css/splide-core.min.css'
 
 import BaseComponent from 'bootstrap/js/src/base-component.js'
 import SelectorEngine from 'bootstrap/js/src/dom/selector-engine'
