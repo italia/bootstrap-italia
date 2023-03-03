583c0b5acc9203287f5333606f28ad562e97810f
Andrea Stagi <stagi.andrea@gmail.com>
2023-03-03 09:31:26 +0000

    chore(release): 2.3.5 :tada:

--- src/js/version.js
+++ src/js/version.js
@@ -1,5 +1,5 @@
 // bootstrap italia version variable
 // useful to check for the current version
 // eslint-disable-next-line no-unused-vars
-const BOOTSTRAP_ITALIA_VERSION = '2.3.4'
+const BOOTSTRAP_ITALIA_VERSION = '2.3.5'
 export default BOOTSTRAP_ITALIA_VERSION
