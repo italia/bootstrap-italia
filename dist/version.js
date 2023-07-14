1eab4d87e80b7badbadda561a11266dff33890a5
astagi <astagi@users.noreply.github.com>
2023-07-14 12:32:13 +0000

    chore: update documentation

--- dist/version.js
+++ dist/version.js
@@ -1,7 +1,7 @@
 // bootstrap italia version variable
 // useful to check for the current version
 // eslint-disable-next-line no-unused-vars
-const BOOTSTRAP_ITALIA_VERSION = '2.6.0';
+const BOOTSTRAP_ITALIA_VERSION = '2.6.1';
 
 export { BOOTSTRAP_ITALIA_VERSION as default };
 //# sourceMappingURL=version.js.map
