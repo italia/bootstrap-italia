e67023797a9e5899303342f36d2c50cef8df377f
Andrea Stagi <stagi.andrea@gmail.com>
2024-11-25 10:39:45 +0000

    chore(release): 2.12.0 :tada:

--- src/js/version.js
+++ src/js/version.js
@@ -1,5 +1,5 @@
 // bootstrap italia version variable
 // useful to check for the current version
 
-const BOOTSTRAP_ITALIA_VERSION = '2.11.2'
+const BOOTSTRAP_ITALIA_VERSION = '2.12.0'
 export default BOOTSTRAP_ITALIA_VERSION
