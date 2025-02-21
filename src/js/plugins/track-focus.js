7350bf486ec6ef9bc8f1f075b411827511b7f12e
Andrea Stagi <stagi.andrea@gmail.com>
2025-02-21 18:43:47 +0100

    fix: use data to set focus instead of class
    BREAKING CHANGE: focus is set by data attribute instead of class

--- src/js/plugins/track-focus.js
+++ src/js/plugins/track-focus.js
@@ -7,16 +7,12 @@
  */
 
 const DATA_MOUSE_FOCUS = 'data-focus-mouse'
-const CLASS_NAME_MOUSE_FOCUS = 'focus--mouse'
 
 class TrackFocus {
   constructor() {
     this._usingMouse = false
-
     this._bindEvents()
   }
-
-  // Public
 
   // Private
   _bindEvents() {
@@ -32,14 +28,12 @@ class TrackFocus {
     document.addEventListener('focusin', (evt) => {
       if (this._usingMouse) {
         if (evt.target) {
-          evt.target.classList.add(CLASS_NAME_MOUSE_FOCUS)
           evt.target.setAttribute(DATA_MOUSE_FOCUS, true)
         }
       }
     })
     document.addEventListener('focusout', (evt) => {
       if (evt.target) {
-        evt.target.classList.remove(CLASS_NAME_MOUSE_FOCUS)
         evt.target.setAttribute(DATA_MOUSE_FOCUS, false)
       }
     })
