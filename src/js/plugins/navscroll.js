d41fcbe1150a838253f728f1afccec028ba92b9d
Daniele T <Fupete@users.noreply.github.com>
2025-04-02 11:33:10 +0200

    chore: clean comment

--- src/js/plugins/navscroll.js
+++ src/js/plugins/navscroll.js
@@ -5,7 +5,7 @@ import SelectorEngine from './dom/selector-engine'
 import Manipulator from './dom/manipulator'
 
 import onDocumentScroll from './util/on-document-scroll'
-import NavBarCollapsible from './navbar-collapsible' // XXX <<< Anche il Navscroll usa il navbar-collapsible
+import NavBarCollapsible from './navbar-collapsible' 
 
 import { documentScrollTo } from './util/tween'
 
