c7d3b5e08a66d59d0fe3ef6686c62dfa7a624fb4
Daniele T <Fupete@users.noreply.github.com>
2025-05-30 09:18:20 +0200

    fix: linting

--- docs/assets/src/js/docs.js
+++ docs/assets/src/js/docs.js
@@ -76,7 +76,8 @@
     })
   })
 
-  anchors.options = { // XXX This add complexity for Screen Reader testing (example: navbar mobile)
+  anchors.options = {
+    // XXX This add complexity for Screen Reader testing (example: navbar mobile)
     icon: '#',
   }
   anchors.add('.bd-content > h2, .bd-content > h3, .bd-content > h4, .bd-content > h5')
