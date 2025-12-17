5bb6f1863d5566590fe8c04e6f1a1ec6012e78c5
Andrea Stagi <stagi.andrea@gmail.com>
2025-12-17 14:32:16 +0100

    feat: extract with prefix

--- extract_custom_properties.py
+++ extract_custom_properties.py
@@ -11,7 +11,7 @@ OUTPUT_JSON = os.path.join('api', 'custom_properties.json')
 glob_vars = []
 
 def clean_variable(var):
-    return var.replace(':', '').replace('$', '')
+    return var #.replace(':', '')
 
 # Look for all available variables
 
@@ -20,7 +20,7 @@ for root, dirs, files in os.walk(SCSS_BASE_PATH):
         if file.endswith(".scss"):
             css_file_to_inspect = os.path.join(root, file)
             with open(css_file_to_inspect, "r") as f:
-              vars = [re.findall(r'^\$.*:',line) for line in f]
+              vars = [re.findall(r'\s--#{\$prefix}.*\:',line) for line in f]
               vars = (functools.reduce(operator.iconcat, vars, []))
               glob_vars.extend(vars)
 
@@ -29,12 +29,13 @@ for root, dirs, files in os.walk(SCSS_BASE_PATH):
 mapped_vars = {}
 
 for var in glob_vars:
+    var = var.replace("--#{$prefix}", "")
     prefixes = var.split('-')
     if len(prefixes) > 1:
         prefix = clean_variable(prefixes[0])
         if prefix not in mapped_vars:
             mapped_vars[prefix] = []
-        mapped_vars[prefix].append(clean_variable(var))
+        mapped_vars[prefix].append(f"--bsi-{clean_variable(var)}")
 
 
 with open(OUTPUT_JSON, "w") as fapi:
