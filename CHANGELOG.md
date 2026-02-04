4c1f69462df4a5139f34ae06d81339359c67b7fc
astagi <astagi@users.noreply.github.com>
2026-02-04 10:37:57 +0000

    chore: update documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## [2.17.3](https://github.com/italia/bootstrap-italia/compare/v2.17.2...v2.17.3) (2026-02-04)
+
+* security update
+
+
+
 ## [2.17.2](https://github.com/italia/bootstrap-italia/compare/v2.17.1...v2.17.2) (2026-01-19)
 
 
--- dist/version.js
+++ dist/version.js
@@ -8,7 +8,7 @@
 
 // NOTE:bootstrap italia version variable, useful to check for the current version
 
-const BOOTSTRAP_ITALIA_VERSION = '2.17.2';
+const BOOTSTRAP_ITALIA_VERSION = '2.17.3';
 
 export { BOOTSTRAP_ITALIA_VERSION as default };
 //# sourceMappingURL=version.js.map
